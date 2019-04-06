020ac3d5af2d1e35d6086a5216ec2b62e156e468
Pilar Mireles <pilar.2m@hotmail.com>
2019-04-06 16:49:29 -0400

    javascript validation

--- js/CallAPI.js
+++ js/CallAPI.js
@@ -1,8 +1,19 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+    var divsToHide = document.getElementById("alert");
+    var alertdanger= document.getElementById("alertdanger");
+    divsToHide.style.display="none";
+    alertdanger.style.display="none";
+});
+function register (){
 
     var Email=document.getElementById("Email").value;
     var Name=document.getElementById("Complete-name").value;
     var Phone=document.getElementById("contact-phone").value;
+    var divsToHide = document.getElementById("alert");
+    var alertdanger= document.getElementById("alertdanger");
+    
+    alertdanger.style.display="none";
   if (Email!="" && Name!=""&& Phone!="" )
   {
 var user={
@@ -17,10 +28,21 @@ var user={
     data: JSON.stringify(user),
     contentType: "application/json",
     success: function (data) {
+        divsToHide.style.display="block";
+        alertdanger.style.display="none";
+      
     }
 });
+divsToHide.style.display="block";
+setTimeout(function() {
+$('#register-user').modal('hide');
+location.reload();}, 4000);
   }
   else{
-    console.warn('Hi!');
+    alertdanger.style.display="block";
+    setTimeout(function() {
+    alertdanger.style.display="none";
+}, 4000);
+    divsToHide.style.display="none";
   }
-});
+}
