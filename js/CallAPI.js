57f508834b9a5b526e5ec58f772205364f47e2ae
Pilar Mireles <pilar.2m@hotmail.com>
2019-04-11 23:09:12 -0400

    add the image behind the cellphone and pc.

--- js/CallAPI.js
+++ js/CallAPI.js
@@ -1,48 +1,41 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    var divsToHide = document.getElementById("alert");
-    var alertdanger= document.getElementById("alertdanger");
-    divsToHide.style.display="none";
-    alertdanger.style.display="none";
+  var divsToHide = document.getElementById("alert");
+  var alertdanger = document.getElementById("alertdanger");
+  alertdanger.style.display = "none";
 });
-function register (){
+function register() {
 
-    var Email=document.getElementById("Email").value;
-    var Name=document.getElementById("Complete-name").value;
-    var Phone=document.getElementById("contact-phone").value;
-    var divsToHide = document.getElementById("alert");
-    var alertdanger= document.getElementById("alertdanger");
-    
-    alertdanger.style.display="none";
-  if (Email!="" && Name!=""&& Phone!="" )
-  {
-var user={
-  	 'Name': Name,
-     'Email': Email,
-     'Telephone':Phone
-   };
+  var Email = document.getElementById("Email").value;
+  var Name = document.getElementById("Complete-name").value;
+  var Phone = document.getElementById("contact-phone").value;
+  var alertdanger = document.getElementById("alertdanger");
 
-   $.ajax({
-    url: "http://localhost:55799/api/User",
-    type: "POST",
-    data: JSON.stringify(user),
-    contentType: "application/json",
-    success: function (data) {
-        divsToHide.style.display="block";
-        alertdanger.style.display="none";
-      
-    }
-});
-divsToHide.style.display="block";
-//setTimeout(function() {
-$('#register-user').modal('hide');
-//location.reload();}, 4000);
+  alertdanger.style.display = "none";
+  if (Email != "" && Name != "" && Phone != "") {
+    var user = {
+      'Name': Name,
+      'Email': Email,
+      'Telephone': Phone
+    };
+
+    $.ajax({
+      url: "http://localhost:55799/api/User",
+      type: "POST",
+      data: JSON.stringify(user),
+      contentType: "application/json",
+      success: function (data) {
+        alertdanger.style.display = "none";
+
+      }
+    });
+    $('#register-user').modal('hide');
+    $('#MessageAccount').modal('show');
   }
-  else{
-    alertdanger.style.display="block";
-    setTimeout(function() {
-    alertdanger.style.display="none";
-}, 4000);
-    divsToHide.style.display="none";
+  else {
+    alertdanger.style.display = "block";
+    setTimeout(function () {
+      alertdanger.style.display = "none";
+    }, 4000);
   }
 }
